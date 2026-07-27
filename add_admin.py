382b8b65453f8121bdb085322e8483f69c0602f9
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2026-07-27 16:37:14 +0100

    Fix add_admin script to use role=UserRole.admin

--- add_admin.py
+++ add_admin.py
@@ -1,10 +1,10 @@
 import asyncio
 from core.database import AsyncSessionLocal
-from models.orm import User
+from models.orm import User, UserRole
 
 async def main():
     async with AsyncSessionLocal() as db:
-        user = User(email="abbeywilkinson123@gmail.com", is_admin=True)
+        user = User(email="abbeywilkinson123@gmail.com", role=UserRole.admin)
         db.add(user)
         await db.commit()
         print("User added!")
